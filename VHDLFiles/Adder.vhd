024633e989d2de8087c6ba3f0ae56973df986a44
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-05-28 14:34:58 +0200

    Make Adder Component

--- VHDLFiles/Adder.vhd
+++ VHDLFiles/Adder.vhd
@@ -1,1 +1,19 @@
-hi
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.numeric_std.all;
+
+ENTITY adder IS
+	PORT(
+        Op1: IN std_logic_vector (31 DOWNTO 0);
+        Op2: IN std_logic_vector (31 downto 0);
+        result: OUT std_logic_vector (31 downto 0)
+		);
+END ENTITY adder;
+
+ARCHITECTURE arch_adder OF adder IS
+	BEGIN
+    result <= std_logic_vector(unsigned(Op1) + unsigned(Op2));
+		
+		
+END arch_adder;
